66687b671ce1f80e13eb984f05a5684a4d2437e7
Corentin <corentin.moiszan1@gmail.com>
2019-01-31 13:30:20 +0100

    Fix header + Page A propos

--- www/about.php
+++ www/about.php
@@ -5,7 +5,7 @@ $description = "Page de ".$title." de becycle";
 ?>
 <?php include_once('_inc/header.php')  ?>
 <div id="about">
-  <div id="startup">
+  <div class="abt" id="startup">
     <div>
       <h2>La start-up</h2>
       <div class="border"></div>
@@ -14,12 +14,25 @@ $description = "Page de ".$title." de becycle";
     </div>
     <img src="img/work.jpg">
   </div>
-  <div>
-    <h2>L'Equipe</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu facilisis lorem. Proin sit amet odio sagittis, luctus nulla eu, ultricies justo. Aliquam tristique molestie neque, non congue mi facilisis quis. Suspendisse ut malesuada arcu. Proin tempus mauris in arcu finibus, id mollis risus efficitur. Aliquam euismod ullamcorper elementum.</p>
+  <div class="abt">
+    <div id="team">
+      <h2>L'Equipe</h2>
+      <div class="border"></div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu facilisis lorem. Proin sit amet odio sagittis, luctus nulla eu, ultricies justo. Aliquam tristique molestie neque, non congue mi facilisis quis. Suspendisse ut malesuada arcu. Proin tempus mauris in arcu finibus, id mollis risus efficitur. Aliquam euismod ullamcorper elementum.</p>
+    </div>
   </div>
-  <h2>Nos partenaires</h2>
-  <h2>Nous rejoindre</h2>
+  <div class="abt">
+    <div id="join">
+      <h2>Nous rejoindre</h2>
+      <div class="border"></div>
+    </div>
+  </div>
+  <div class="abt">
+    <div id="partner">
+      <h2>Nos partenaires</h2>
+    </div>
+  </div>
+
 </div>
 
 <?php include_once('_inc/footer.php')  ?>
