b158fd8ae39aef403408ff87f9ae0c9e70529c46
Corentin <corentin.moiszan1@gmail.com>
2019-02-26 11:51:10 +0100

    Legende carte monde

--- www/monde.php
+++ www/monde.php
@@ -9,5 +9,6 @@ $title = "Becycle | Le vélo dans le monde";
   <p>Copenhague (Danemark), Amsterdam (Pays-Bas), mais aussi Strasbourg ou Nantes... De plus en plus de villes, principalement en Europe, développent des « politiques cyclables » pour réinstaller les vélos dans notre paysage urbain, chacune à son échelle.
 Ces politiques cyclables, c’est la mise en place de légères modifications dans l’environnement urbain, pour bousculer les habitudes des habitants et les inviter à en adopter de nouvelles. Une généralisation à 30 km/h, des aménagements (principalement des pistes cyclables), mais aussi des offres de stationnement en développement (des parkings dédiés, des locaux sécurisés et souvent privatisés...) sont mis en place.</p>
   <object id="map" type="image/svg+xml" data="img/world.svg"></object>
+  <p class="legend">Classement des 20 villes offrant les meilleures conditions de déplacement aux cyclistes.</p>
 </div>
 <?php include_once('_inc/footer.php')  ?>
